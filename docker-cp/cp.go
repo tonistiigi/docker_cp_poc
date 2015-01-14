bb9fdabe539f7c701cfe8373086e1a5c83771066
Tonis Tiigi <tonistiigi@gmail.com>
2015-01-14 20:35:20 +0200

    cp path check and rename

--- docker-cp/cp.go
+++ docker-cp/cp.go
@@ -6,6 +6,10 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path"
+	"strings"
+
+	"github.com/docker/docker/vendor/src/code.google.com/p/go/src/pkg/archive/tar"
 )
 
 func main() {
@@ -32,9 +36,31 @@ func main() {
 	}
 	go io.Copy(os.Stderr, packStderr)
 
-	// todo: Exceptions for:
+	// todo: paths should be cleaned with suffixes kept
+
+	// <magic>
+
+	_, fbase := path.Split(from)
+	_, tbase := path.Split(to)
+
 	// /foo -> /bar
 	// /foo/ -> /bar
+	if fbase != "." && tbase != "" && tbase != "." {
+		log.Println("check-destination", to)
+		if !destinationExists(to) {
+			log.Println("destination not found")
+			to = path.Dir(to)
+
+			if fbase == "" {
+				fbase = path.Base(from)
+			}
+
+			packout = rename(packout, fbase, tbase)
+			log.Println("rename", fbase, tbase)
+		}
+	}
+
+	// </magic>
 
 	unpack := exec.Command("docker-unpack", to)
 	log.Println("docker-unpack", to)
@@ -47,6 +73,10 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	if err := pack.Start(); err != nil {
+		log.Fatal(err)
+	}
 	if err := unpack.Start(); err != nil {
 		log.Fatal(err)
 	}
@@ -56,4 +86,59 @@ func main() {
 		log.Fatal(err) // this doesn't check dest errors properly. Broken pipe.
 	}
 
+	unpackin.Close()
+	if err = unpack.Wait(); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+// normally this would check for 404
+func destinationExists(dest string) bool {
+	unpack := exec.Command("docker-unpack", dest)
+	err := unpack.Run()
+	return err == nil
+}
+
+func rename(in io.ReadCloser, from, to string) io.ReadCloser {
+	r, w := io.Pipe()
+
+	// should wait until someone actually reads
+	go func() {
+		reader := tar.NewReader(in)
+		writer := tar.NewWriter(w)
+
+		for {
+			hdr, err := reader.Next()
+			if err == io.EOF {
+				w.Close()
+				return
+			}
+			if err != nil {
+				w.CloseWithError(err)
+				return
+			}
+
+			hdr.Name = path.Clean(hdr.Name)
+			if hdr.Name == from || strings.HasPrefix(hdr.Name, from+"/") {
+				hdr.Name = strings.Replace(hdr.Name, from, to, 1)
+			}
+
+			err = writer.WriteHeader(hdr)
+			if err != nil {
+				w.CloseWithError(err)
+				return
+			}
+
+			if hdr.Size != 0 {
+				_, err = io.Copy(writer, reader)
+				if err != nil {
+					w.CloseWithError(err)
+					return
+				}
+			}
+		}
+	}()
+
+	return r
 }
